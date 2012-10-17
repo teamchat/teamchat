d76124083cbab00df32e5e54068096652d4681f0
Nic Ferier <nic@ferrier.me.uk>
2012-10-17 07:01:01 +0100

    remove the emacs puppet class, replaced by a separate module tree

--- puppet/modules/shoesoff/manifests/init.pp
+++ puppet/modules/shoesoff/manifests/init.pp
@@ -1,3 +1,10 @@
 class shoesoff {
-      
+      include emacs
+
+      $local_archive = "/vagrant/shoesoff-elpa"
+
+      exec { "install-talk-app":
+         require => Service["emacs"],
+         command => "/etc/init.d/emacs install talk $local_archive",
+      }
 }
