965778c6a11918db1158a23d41d587dd2846e2af
Nic Ferier <nic@ferrier.me.uk>
2012-10-29 17:15:22 +0000

    record the customizations we need for a sane emacs environment.

---
+++ emacs-customizations.el
@@ -0,0 +1,8 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.                                                                                                         
+ ;; If you edit it by hand, you could mess it up, so be careful.                                                                                      
+ ;; Your init file should contain only one such instance.                                                                                             
+ ;; If there is more than one, they won't work right.                                                                                                 
+ '(elnode-error-log-to-messages nil)
+ '(revert-without-query (quote ("~/\\.emacs.d/elpa/.*")))
+ '(talkapp-db-dir "/home/emacs/talkapp-dbs"))
