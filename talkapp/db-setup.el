5020eaa2fcec60c9e56e59ebca83749b51b033fb
Nic Ferier <nic@ferrier.me.uk>
2012-10-29 15:49:02 +0000

    initial thoughtworks org

---
+++ talkapp/db-setup.el
@@ -0,0 +1,9 @@
+;; db changes
+
+(talkapp/org-new "thoughtworks"
+                 :match-host "twirc.ferrier.me.uk"
+                 :domain-name "thoughtworks.com"
+                 :irc-server "irc.jbx.cc"
+                 :primary-channel "#thoughtworks")
+
+;; db-setup.el ends here
