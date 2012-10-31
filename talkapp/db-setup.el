d47a1e9016c8ceb316647dd46d484080e98059be
Nic Ferier <nic@ferrier.me.uk>
2012-10-31 17:29:26 +0000

    install the hackernews organization for the demo.

--- talkapp/db-setup.el
+++ talkapp/db-setup.el
@@ -5,7 +5,7 @@
   (talkapp/org-new "thoughtworks"
                    :match-host "twirc.ferrier.me.uk"
                    :domain-name "thoughtworks.com"
-                   :irc-server "irc.jbx.cc"
+                   :irc-server "irc.jbx.cc:6667"
                    :primary-channel "#thoughtworks")
   (db-map (lambda (key value)
             (db-put
@@ -15,6 +15,17 @@
           talkapp/user-db)
   ;; Mark the change
   (db-put "20120629160609914467000"
+          '(("timestamp" . "20120629160609914467000"))
+          talkapp/db-change-db))
+
+;; Add the hn org
+(unless (db-get "20120131130158357629000" talkapp/db-change-db)
+  (talkapp/org-new "hn"
+                   :match-host "hn.teamchat.net"
+                   :domain-name "hn.teamchat.net"
+                   :irc-server "irc.teamchat.net:6901"
+                   :primary-channel "#hn")
+  (db-put "20120131130158357629000"
           '(("timestamp" . "20120629160609914467000"))
           talkapp/db-change-db))
 
