d1353a4c46948f289177dea621f24491889c7365
Nic Ferier <nic@ferrier.me.uk>
2012-11-02 13:25:28 +0000

    make the build a list of commands

--- build.el
+++ build.el
@@ -1,7 +1,9 @@
 ;; Build the bits that need to be built
 
 ;; The emacs puppet stuff
-(shell-command-to-string "cp -r ~/work/emacs-puppet puppet-emacs")
+(defun talkapp-build-puppet ()
+  (interactive)
+  (shell-command-to-string "cp -r ~/work/emacs-puppet puppet-emacs"))
 
 (defconst shoes-off-elpakit
   '("~/work/shoes-off"
@@ -15,15 +17,21 @@
 (defconst talkapp-do-rcirc nil)
 
 ;; The local elpa dir
-(elpakit "~/work/shoes-off-aas/shoesoff-elpa" shoes-off-elpakit)
+(defun talkapp-do-elpakit-to-local ()
+  (interactive)
+  (elpakit "~/work/shoes-off-aas/shoesoff-elpa" shoes-off-elpakit))
 
 ;; the remote elpa
-(let ((remote
-       "/ssh:nferrier@po1.ferrier:irc--talk/shoesoffaas/shoesoff-elpa/"))
-  (delete-directory remote t)
-  (elpakit remote shoes-off-elpakit))
+(defun talkapp-do-elpakit-to-live ()
+  (interactive)
+  (let ((remote
+         "/ssh:nferrier@po1.ferrier:irc--talk/shoesoffaas/shoesoff-elpa/"))
+    (delete-directory remote t)
+    (elpakit remote shoes-off-elpakit)))
 
 ;; Inside your emacs
-(elpakit-eval shoes-off-elpakit)
+(defun talkapp-do-elpakit-eval ()
+  (interactive)
+  (elpakit-eval shoes-off-elpakit))
 
 ;; End
